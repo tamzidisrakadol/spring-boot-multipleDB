c9a6d5b8fcac4c22953c5a2f1b474351dd093456
Tamzid Israk Adol <tamzidisrak12@gmail.com>
2025-01-16 06:51:02 +0600

    Remove all the file

--- src/main/java/com/example/springbootmultipleDB/POSTGRESQL/PostgreSqlController.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootmultipleDB.POSTGRESQL;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class PostgreSqlController {
-}
--- src/main/java/com/example/springbootmultipleDB/SQL/MySqlController.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootmultipleDB.SQL;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class MySqlController {
-
-
-}
